6bf5414a105406fa2132698f406a3ff2bb10d943
Goyhman Peter <pgoyhman@mail.ru>
2023-03-12 15:41:31 +0300

    task01

---
+++ Task01.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+// Заполнить список 10 случайными числами
+// Отсортировать список методом sort() и вывести его на экран
+
+public class Task01 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        ArrayList<Integer> arr = new ArrayList<>(10);
+        for (int i = 0; i < 10; i++) {
+            arr.add(random.nextInt(10));
+        }
+        Collections.sort(arr);
+        System.out.println(arr);
+    }
+}
